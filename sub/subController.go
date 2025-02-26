da9945e6a368ce01864ebadea4ef613742c84e12
Blur02 <loookyar@gmail.com>
2025-02-26 14:53:41 +0300

    subController.go

--- sub/subController.go
+++ sub/subController.go
@@ -82,7 +82,7 @@ func (a *SUBController) subs(c *gin.Context) {
 		// Add headers
 		c.Writer.Header().Set("Subscription-Userinfo", header)
 		c.Writer.Header().Set("Profile-Update-Interval", a.updateInterval)
-		c.Writer.Header().Set("Profile-Title", Blur_Vpn_bot)
+		c.Writer.Header().Set("Profile-Title", "tg:@BLUR_VPN_BOT 🌐")
 
 		if a.subEncrypt {
 			c.String(200, base64.StdEncoding.EncodeToString([]byte(result)))
@@ -116,7 +116,7 @@ func (a *SUBController) subJsons(c *gin.Context) {
 		// Add headers
 		c.Writer.Header().Set("Subscription-Userinfo", header)
 		c.Writer.Header().Set("Profile-Update-Interval", a.updateInterval)
-		c.Writer.Header().Set("Profile-Title", Blur_Vpn_Bot)
+		c.Writer.Header().Set("Profile-Title", "tg:@BLUR_VPN_BOT 🌐")
 
 		c.String(200, jsonSub)
 	}
